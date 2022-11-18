58f543e6f22f172bcfcb48aabf0920f93aa14ef6
Collin Morudi <collinmorudi@gmail.com>
2022-11-18 15:06:51 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a Printf Project... more content to be added
---
+++ print.c
@@ -0,0 +1,126 @@
+#include "main.h"
+
+/**
+ * check_buffer_overflow - check buffer and print what's on the
+ * buffer and the set length back to 0
+ * @buffer: buffer holding the string
+ * @len: position in buffer
+ * Return: length of position
+ */
+int check_buffer_overflow(char *buffer, int len)
+{
+	if (len > 1020)
+	{
+		write(1, buffer, len);
+		len = 0;
+	}
+	return (len);
+}
+
+/**
+ * _printf - custom printf function
+ * @format: string with all identifiers
+ * Return: strings with identifiers expanded
+ */
+int _printf(const char *format, ...)
+{
+	int len = 0, total_len = 0;
+	int i = 0, j = 0;
+	va_list list;
+	char *buffer;
+	char *str;
+	char* (*f)(va_list);
+
+	if (format == NULL)
+		return (-1);
+
+	buffer = create_buffer();
+	if (buffer == NULL)
+		return (-1);
+
+	va_start(list, format);
+
+	while (format[i] != '\0')
+	{
+		if (format[i] != '%')
+		{
+			len = check_buffer_overflow(buffer, len);
+			buffer[len++] = format[i++];
+			total_len++;
+		}
+		else
+		{
+			i++;
+			if (format[i] == '\0')
+			{
+				va_end(list);
+				free(buffer);
+				return (-1);
+			}
+			if (format[i] == '%')
+			{
+				len = check_buffer_overflow(buffer, len);
+				buffer[len++] = format[i];
+				total_len++;
+			}
+			else
+			{
+				f = get_func(format[i]);
+				if (f == NULL)
+				{
+					len = check_buffer_overflow(buffer, len);
+					buffer[len++] = '%'; total_len++;
+					buffer[len++] = format[i]; total_len++;
+				}
+				else
+				{
+					str = f(list);
+					if (str == NULL)
+					{
+						va_end(list);
+						free(buffer);
+						return (-1);
+					}
+					if (format[i] == 'c' && str[0] == '\0')
+					{
+						len = check_buffer_overflow(buffer, len);
+						buffer[len++] = '\0';
+						total_len++;
+					}
+					j = 0;
+					while (str[j] != '\0')
+					{
+						len = check_buffer_overflow(buffer, len);
+						buffer[len++] = str[j];
+						total_len++; j++;
+					}
+					free(str);
+				}
+			} i++;
+		}
+	}
+	write_buffer(buffer, len, list);
+	return (total_len);
+}
+
+/**
+ * main - main
+ * Return: 0 on successful run
+ */
+int main(void)
+{
+	_printf("\n\n\nHere's some examples of what you could do with this custom_printf function!\n\n\n");
+	sleep(1);
+	_printf("\nPrinting Strings, Characters, and Numbers...... %s %c%drld\n\n", "Hello", 'W', 0);
+	sleep(1);
+	_printf("Printing Reverse...... %r \n\n", "Hello");
+	sleep(1);
+	_printf("Printing Binary (base 2)...... %b \n\n", "Hello");
+	sleep(1);
+	_printf("Printing Octal (base 8)...... %o \n\n", "Hello");
+	sleep(1);
+	_printf("Printing Rot13 (encrypt)...... %R \n\n", "Hello");
+	sleep(1);
+	_printf("\n\n             = )                  \n\n\n");
+	return (0);
+}
